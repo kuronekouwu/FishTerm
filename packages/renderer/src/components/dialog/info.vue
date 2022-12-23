88aa8e4c7fd526205e8599aa1bd1bba0dea57278
M307 (Mac) <me@m307.dev>
2022-12-24 02:08:47 +0700

    Add new info

--- packages/renderer/src/components/dialog/info.vue
+++ packages/renderer/src/components/dialog/info.vue
@@ -1,14 +1,32 @@
+<script lang="ts" setup>
+import iconUrl from '../../../assets/logo.svg?url';
+
+const version = import.meta.env.VITE_APP_VERSION;
+</script>
+
 <template>
     <div class="text-center dark:text-white text-dark space-y-3">
-        <div id="logo" class="space-y-5">
-            <img class="mx-auto rounded-2xl" src="https://images-ext-2.discordapp.net/external/TwT8zovtF8ciu-Sf2F92B6jMqZtT2fdtkEt7wA926iU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1051062550210564127/b661038c2ffcf0fe172029aaa59b7f36.png" />
-            <h1 class="text-3xl">Fish Term 🐟</h1>
+        <div
+            id="logo"
+            class="space-y-3"
+        >
+            <img
+                class="mx-auto rounded-2xl w-[256px] h-[256px]"
+                :src="iconUrl"
+            />
+
+            <div class="space-y-2">
+                <h1 class="text-3xl">Fish Term 🐟</h1>
+                <p class="text-sm">{{ version }}</p>
+            </div>
         </div>
-        <div id="desc" class="block">
-            <p class="text-sm">Version: 0.0.1 (alpha)</p>
-            <p class="text-sm">Lastest: 22 Dec 2022</p>
-            <p class="text-sm">Developer: M-307</p>
-            <p class="text-sm">Speical thanks: Wazt, nSys</p>
+        <div
+            id="desc"
+            class="block"
+        >
+            <p class="text-sm">Developer (Main): @M-307</p>
+            <p class="text-sm">Developer (2nd): @Maythiwat, @nSys</p>
+            <p class="text-sm">Tester: -</p>
         </div>
     </div>
 </template>
