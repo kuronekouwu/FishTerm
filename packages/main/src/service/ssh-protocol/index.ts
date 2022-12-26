3a0a6b43141bd83397afee95111a20379de6016e
M307 (Mac) <me@m307.dev>
2022-12-26 20:13:14 +0700

    Fix is writeable

--- packages/main/src/service/ssh-protocol/index.ts
+++ packages/main/src/service/ssh-protocol/index.ts
@@ -1,6 +1,6 @@
 import {Socket} from 'net';
 import EventEmitter from 'eventemitter3';
-import { SSHProtocol } from '../ssh'
+import {SSHProtocol} from '../ssh';
 
 export class SSH extends EventEmitter {
     private readonly socket: Socket;
@@ -12,49 +12,52 @@ export class SSH extends EventEmitter {
     }
 
     init() {
-        this.socket.on("connect", () => {
+        this.socket.on('connect', () => {
             const ssh = new SSHProtocol();
-            this.emit('socket.ready')
-            this.socket.on("data", (data: Buffer) => {
-                ssh.write(data);
+            this.emit('socket.ready');
+            this.socket.on('data', (data: Buffer) => {
+                if (this.socket.writable) ssh.write(data);
             });
-            this.socket.on("close", () => this.emit("socket.close"))
-    
+            this.socket.on('close', () => this.emit('socket.close'));
+
             // SSH Protocol
             ssh.on('data', (data: Buffer) => {
-                this.socket.write(data);
-            })
-            ssh.once('verify', (verify) => {
+                if (this.socket.writable) this.socket.write(data);
+            });
+            ssh.once('verify', verify => {
                 return verify(true);
-            })
+            });
             ssh.once('end', () => {
                 this.emit('ssh.end');
                 this.socket.destroy();
             });
             ssh.once('error', n => {
-                this.emit('ssh.error', n)
+                this.emit('error', n);
             });
-            ssh.once(("auth_failed"), () => {
-                this.emit("ssh.auth.failed")
-            })
-            ssh.once('close', () => this.emit('ssh.close'));
-    
+            ssh.once('auth_failed', () => {
+                this.emit('ssh.auth.failed');
+            });
+            ssh.once('close', () => {
+                this.emit('ssh.close');
+                // Destory socket
+                this.socket.destroy();
+            });
+
             // SSH2 Data
-            ssh.on("password_request", (type, cb) => this.emit('ssh.password.request', type, cb))
-            ssh.on("connected", (client) => this.emit("ssh.connect", client))
+            ssh.on('password_request', (type, cb) => this.emit('ssh.password.request', type, cb));
+            ssh.on('connected', client => this.emit('ssh.connect', client));
         });
-        this.socket.once("timeout", () => {
-            this.emit("ssh.timeout")
-            this.socket.destroy()
-        })
+        this.socket.once('timeout', () => {
+            this.emit('ssh.timeout');
+            this.socket.destroy();
+        });
     }
 
-    listen(host: string, port: number = 22){
+    listen(host: string, port: number = 22) {
         this.socket.setTimeout(1000 * 10);
         this.socket.connect({
             host: host,
-            port: port
-        })
-
+            port: port,
+        });
     }
 }
